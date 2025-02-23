35d09f9b7a51fbfb5b12d7cb87030f03c8efeeba
Diego Flores Avalos <129472574+DiegoFA04@users.noreply.github.com>
2025-02-22 22:45:22 -0500

    example react component

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+    <HolaMundo />
+  </StrictMode>
+);
+
+function HolaMundo() {
+  return (
+    <>
+      <h1>Hola Mundo</h1>
+      <div>Estiamdo</div>
+    </>
+  );
+}
