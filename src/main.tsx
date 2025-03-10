7b5229dfb1c9499f2aa5e73012627ec74212b1db
Diego Flores <u20221b394@upc.edu.pe>
2025-03-09 20:37:54 -0500

    using props

--- src/main.tsx
+++ src/main.tsx
@@ -6,15 +6,24 @@ import App from "./App.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-    <HolaMundo />
+    <HolaMundo name="Ricardo" age={22} />
   </StrictMode>
 );
 
-export function HolaMundo() {
+export function HolaMundo(props: IProps) {
+  const firstName: string = "John";
+  const lastName: string = "Doe";
+  console.log(props);
   return (
     <>
-      <h1 className="new">Hola Mundo Nuevo</h1>
-      <div className="new">Estimado</div>
+      <h1 className="new">Hola {firstName} {lastName} </h1>
+      <h2 className="new">Name: {props.name} Age: {props.age} </h2>
+      <div className="new">The Date is {new Date().getDate() }</div>
     </>
   );
+}
+
+interface IProps { 
+  name: string;
+  age: number;
 }
