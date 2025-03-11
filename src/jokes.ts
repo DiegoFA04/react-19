6f224c972bc1fc3e00f56eec475a821f32d88b4f
Diego Flores <u20221b394@upc.edu.pe>
2025-03-10 22:33:23 -0500

    render arrays

---
+++ src/jokes.ts
@@ -0,0 +1,10 @@
+export default [
+    {
+        joke: "Why did the scarecrow win an award?",
+        punchline: "Because he was outstanding in his field.",
+    },
+    {
+        joke: "Why did the scarecrow win an award?",
+        punchline: "Because he was outstanding in his field.",
+    },
+];
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import  jokes  from "./jokes";
 import "./index.css";
 import App from "./App.tsx";
 
@@ -13,17 +14,29 @@ createRoot(document.getElementById("root")!).render(
 export function HolaMundo(props: IProps) {
   const firstName: string = "John";
   const lastName: string = "Doe";
+  const jokeElements = jokes.map((joke) => (
+    <div>
+      <h1>{joke.joke}</h1>
+      <p>{joke.punchline}</p>
+    </div>
+  ));
   console.log(props);
   return (
     <>
-      <h1 className="new">Hola {firstName} {lastName} </h1>
-      <h2 className="new">Name: {props.name} Age: {props.age} </h2>
-      <div className="new">The Date is {new Date().getDate() }</div>
+      <h1 className="new">
+        Hola {firstName} {lastName}{" "}
+      </h1>
+      <h2 className="new">
+        Name: {props.name} Age: {props.age}{" "}
+      </h2>
+      <div className="new">The Date is {new Date().getDate()}</div>
+      {jokeElements}
     </>
   );
 }
 
-interface IProps { 
+interface IProps {
   name: string;
   age: number;
 }
+
